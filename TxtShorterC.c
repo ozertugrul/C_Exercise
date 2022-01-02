b05a32f05ed9de3ee3d4c220668557ebe377f871
Muhammet Ertuğrul Özer <96636061+ozertugrul@users.noreply.github.com>
2022-01-03 01:36:19 +0300

    Update and rename week7-212002002.c to TxtShorterC.c

--- TxtShorterC.c
+++ TxtShorterC.c
@@ -3,15 +3,16 @@
 #include<ctype.h>
 /*
 
-ilk harfi buyut ve bastir
-dongu baslat uzunluga kadar
-if bosluk; i+1 buyut ve bastir 
+GIRILEN METINDEKI KELIMELERI ILK HARFINE GORE KISALTIR
+SIMPLE:
+//>> MUHAMMET ERTUGRUL OZER ----> MEO
+//>> MUSTAFA KEMAL ATATURK ----> MKA
 
 */
 main(){
     char txt[99999];
     int uzunluk;
-    printf("LUTFEN KISALTMAK ISTEDIGINIZ KURUMUN ADINI GIRIN :\n");
+    printf("LUTFEN KISALTMAK ISTEDIGINIZ METINI GIRINIZ :\n");
     gets(txt);
     printf("%c", toupper(txt[0]));
     uzunluk=strlen(txt);
